0ab1eaabb166a2875f5b472b27d0f65eb827990a
Maximiliano Cóppola <maxicopp@outlook.com>
2023-09-30 01:50:11 -0300

    Arrays - Arreglos

---
+++ 01-bases/tipos/arrays.ts
@@ -0,0 +1,20 @@
+(() => {
+  //   const numbers: (string | number | boolean)[] = [
+  //     1,
+  //     2,
+  //     3,
+  //     4,
+  //     5,
+  //     '6',
+  //     7,
+  //     8,
+  //     9,
+  //     10,
+  //   ];
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const villians = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+
+  villians.forEach((villian) => {
+    console.log(villian.toUpperCase());
+  });
+})();
