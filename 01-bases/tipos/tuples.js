9f1fcbd9b261adc98da69df6897757fed6b8217d
Maximiliano Cóppola <maxicopp@outlook.com>
2023-09-30 01:57:04 -0300

    Tuples - Tuplas

---
+++ 01-bases/tipos/tuples.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    const hero = ['Dr Strange', 100, true];
+    console.log(hero);
+    hero[0] = 'Iron Man';
+    hero[1] = 50;
+    hero[2] = false;
+    console.log(hero);
+})();
