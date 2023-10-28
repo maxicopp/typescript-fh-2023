214295b9ce293a696aa9bd8016295442093e4219
Maximiliano Cóppola <maxicopp@outlook.com>
2023-10-28 13:25:13 -0300

    Forma corta de asignar propiedades

--- 01-bases/classes/basica.ts
+++ 01-bases/classes/basica.ts
@@ -1,17 +1,18 @@
 (() => {
   class Avenger {
-    private name: string;
-    private team: string;
-    public realName?: string;
     static avgAge: number = 35;
 
-    constructor(name: string, team: string, realName?: string) {
-      this.name = name;
-      this.team = team;
-      this.realName = realName;
+    constructor(
+      private name: string,
+      private team: string,
+      public realName?: string,
+      avgAge: number = 55
+    ) {
+      Avenger.avgAge = avgAge;
     }
   }
 
-  const antman: Avenger = new Avenger('Antman', 'Capitan');
+  const antman: Avenger = new Avenger('Antman', 'Capitan', 'Scott Lang');
   console.log(antman);
+  console.log(Avenger.avgAge);
 })();
--- 01-bases/main.js
+++ 01-bases/main.js
@@ -1,14 +1,16 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, team, realName, avgAge = 55) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+            Avenger.avgAge = avgAge;
         }
     }
     Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'Capitan');
+    const antman = new Avenger('Antman', 'Capitan', 'Scott Lang');
     console.log(antman);
+    console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
