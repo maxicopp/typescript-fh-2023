37badcbd3cb56a9aa243bc8ea3598ec706e6730b
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-08 02:30:08 -0300

    Export default y exportación con alias

---
+++ 02-ts-node/src/data/powers.ts
@@ -0,0 +1,21 @@
+interface Power {
+  id: number;
+  desc: string;
+}
+
+const powers: Power[] = [
+  {
+    id: 1,
+    desc: 'super strength',
+  },
+  {
+    id: 2,
+    desc: 'fly',
+  },
+  {
+    id: 3,
+    desc: 'x-ray vision',
+  },
+];
+
+export default powers;
--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,5 +1,11 @@
-import { Hero } from './classes/Hero';
+// import { Hero as SuperHero } from './classes/Hero';
+import * as HeroClasses from './classes/Hero';
+import powers from './data/powers';
 
-const ironman = new Hero('Tony Stark', 1, 45);
+const Hero = 123;
+
+const ironman = new HeroClasses.Hero('Tony Stark', 1, 45);
 
 console.log(ironman);
+
+console.log(powers);
