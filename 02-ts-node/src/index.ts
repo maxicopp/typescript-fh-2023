080fd4a8db3ce4ec0ad4b19bdf80d88c10671361
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-13 19:55:24 -0300

    Funciones genéricas

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,4 +1,4 @@
-import { genericFunction, printObject } from './generics/generics';
+import { genericFunction, genericFunctionArrow } from './generics/generics';
 
 // printObject({ name: 'Tony', age: 45 });
 // printObject([1, 2, 3, 4, 5]);
@@ -6,5 +6,9 @@ import { genericFunction, printObject } from './generics/generics';
 // printObject(new Date());
 // printObject('Hello');
 
+const name: string = 'Max';
+
 console.log(genericFunction(3.141618).toFixed(2));
-console.log(genericFunction('Hello World').toFixed(2));
+console.log(genericFunction(name).toUpperCase());
+console.log(genericFunction([1, 2, 3, 4, 5]).find((n) => n > 2));
+console.log(genericFunctionArrow(3.141618).toFixed(2));
