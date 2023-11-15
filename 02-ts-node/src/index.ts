0ac143ed53f7f1293b7392f04ff72cdeb133f4fa
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-15 00:51:37 -0300

    Ejemplo de función genérica en acción

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,4 +1,6 @@
 import { genericFunction, genericFunctionArrow } from './generics/generics';
+import { Hero } from './interfaces/hero';
+import { Villain } from './interfaces/villain';
 
 // printObject({ name: 'Tony', age: 45 });
 // printObject([1, 2, 3, 4, 5]);
@@ -6,9 +8,17 @@ import { genericFunction, genericFunctionArrow } from './generics/generics';
 // printObject(new Date());
 // printObject('Hello');
 
-const name: string = 'Max';
+// const name: string = 'Max';
 
-console.log(genericFunction(3.141618).toFixed(2));
-console.log(genericFunction(name).toUpperCase());
-console.log(genericFunction([1, 2, 3, 4, 5]).find((n) => n > 2));
-console.log(genericFunctionArrow(3.141618).toFixed(2));
+// console.log(genericFunction(3.141618).toFixed(2));
+// console.log(genericFunction(name).toUpperCase());
+// console.log(genericFunction([1, 2, 3, 4, 5]).find((n) => n > 2));
+// console.log(genericFunctionArrow(3.141618).toFixed(2));
+
+// const deadpool = {
+//   name: 'Deadpool',
+//   realName: 'Wade Winston Wilson',
+//   dangerLevel: 130,
+// };
+
+// console.log(genericFunctionArrow<Villain>(deadpool).dangerLevel);
