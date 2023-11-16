bb5a642f230d593d289b1dfa274a5ac36f693617
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-16 03:15:36 -0300

    Creando un Rest API con Express

--- 03-express-api/index.js
+++ 03-express-api/index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Hello World!!!',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
