b03c98185a97071cc8cd63f172ed5f228a1ed2d2
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-16 01:06:58 -0300

    Decoradores de clases

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,12 +1,5 @@
-import { getPokemon } from './generics/get-pokemon';
+import { Pokemon } from './decorators/pokemon-class';
 
-getPokemon(2)
-  .then((pokemon) => {
-    console.log(pokemon.sprites.front_default);
-  })
-  .catch((error) => {
-    console.error(error);
-  })
-  .finally(() => {
-    console.log('Finally');
-  });
+const charmander = new Pokemon('Charmander');
+
+console.log(charmander);
