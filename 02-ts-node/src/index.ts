50c3e39b8d2480bf122baaa0fef3be226938c887
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-16 02:35:55 -0300

    Decoradores de propiedades

--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -5,4 +5,9 @@ const charmander = new Pokemon('Charmander');
 // (Pokemon.prototype as any).customName = 'Pikachu';
 
 // console.log(charmander.savePokemonToDB(4));
-charmander.savePokemonToDB(3);
+// charmander.savePokemonToDB(3);
+
+const myUrl = 'https://google.com';
+charmander.publicApi = myUrl;
+
+console.log(charmander);
